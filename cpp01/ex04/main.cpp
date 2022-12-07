c601c14b83acbdce1148e18c2e328e5b0fc1c787
zel-hach <zineb.ell.zee@gmail.com>
2022-12-07 19:03:43 +0100

    update in ex04 cpp01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,17 +2,17 @@
 
 int main(int argc ,char **argv)
 {
-    if (argc > 1)
+    if (argc >= 1)
     {
         std :: ifstream infile;
         std :: ofstream outfile;
 
-        outfile.open(argv[2]);
+        outfile.open("test.replace", std::fstream::in | std::fstream::out | std::fstream::app);
         infile.open(argv[1]);
         std ::string line;
         while (getline(infile,line))
         {
-            outfile << ReplaceFile(line, argv[3] ,argv[4]);
+            outfile << ReplaceFile(line, argv[2] ,argv[3]);
             outfile << std ::endl;
         }
     }
